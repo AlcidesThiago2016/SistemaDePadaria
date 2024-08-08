07f8a93b58c4ac8c7bc2ea07662ae37a4d31f933
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-08-08 14:27:20 -0300

    Create class Venda.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,17 @@
 package org.example;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+
+import org.example.model.Itens;
+
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("-*-*-*-*Sistema - Padaria DevFront-*-*-*-*");
 
-
+        Itens paoFrances = new Itens("Pão Frances",10,1);
+        Itens paoDoceDeCoco = new Itens("Pão Doce",5.20,2);
+        Itens paoDoceComGoiaba = new Itens("Pão Doce com Goiabada",6.30,3);
+        Itens paoCarteiro = new Itens("Pão Carteiro",4.20,4);
+        Itens paoTatu = new Itens("Pão Doce Tatuzinho",4.10,5);
 
     }
 }
---
+++ src/main/java/org/example/model/Venda.java
@@ -0,0 +1,35 @@
+package org.example.model;
+
+import java.util.ArrayList;
+
+public class Venda {
+    ArrayList<Itens> paes;
+
+    public Venda(){
+        this.paes = new ArrayList<>();
+    }
+
+    public void adicionarItem(Itens item){
+        paes.add(item);
+    }
+
+    public void removerItem(int codigo){
+        String itemRemovido = "";
+        for(int i=0; i<paes.size(); i++){
+            if(paes.get(i).getCodigo() == codigo){
+                itemRemovido = paes.get(i).getNome();
+                paes.remove(i);
+                i= paes.size();
+            }
+        }
+        System.out.println("item " + itemRemovido + " removido com sucesso");
+    }
+    
+    public void calcularTotal(){
+        double total = 0;
+        for(int i=0; i<paes.size(); i++){
+            total += paes.get(i).getPreco();
+        }
+        System.out.println("Total a pagar R$: " + total);
+    }
+}
